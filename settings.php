7fc87fd85291e4d9005d0ba7849892b14934dcd0
j19201 <j19201rs@edu.tuis.ac.jp>
2022-07-04 16:40:33 +0900

    バージョン変更

--- settings.php
+++ settings.php
@@ -6,12 +6,13 @@ $settings->add(new admin_setting_heading("auth_spamblockbeta/pluginname", "",
     new lang_string("auth_spamblockbetadescription", "auth_spamblockbeta")));
 
 $options = array(
-    new lang_string("no"),
     new lang_string("yes"),
+    new lang_string("no"),
 );
 
 $settings->add(new admin_setting_configselect("auth_spamblockbeta/captcha",
     new lang_string("auth_spamblockbetacaptcha","auth_spamblockbeta"),
-    "有効無効",1,$options,
+    new lang_string("auth_spamblockbetacaptchaswitchdescription","auth_spamblockbeta"),
+    0,$options,
 ));
 
--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = "auth_spamblockbeta";
-$plugin->version = 2022061201;
+$plugin->version = 2022061207;
 ?>
