98ad14effdc36c88c47b0b3d939cee6ac9574d85
j19201 <j19201rs@edu.tuis.ac.jp>
2022-07-04 16:39:36 +0900

    メッセージを変更

--- lang/en/auth_spamblockbeta.php
+++ lang/en/auth_spamblockbeta.php
@@ -3,7 +3,9 @@
 $string["pluginname"] = "spam_block_beta";
 $string["auth_spamblockbetadescription"] = "block spam user plugin";
 $string["auth_spamblockbetacaptcha"] = "enable CAPTCHA auth";
+$string["auth_spamblockbetacaptchaswitchdescription"] = "Enables or disables CAPTCHA authentication. \n It is usually not recommended to disable this.";
 $string["auth_spamblockbetacaptchaheader"] = "CAPTCHA Authentication";
-$string["auth_spamblockbetacaptchadescription"] = "If you are not a robot, select B.";
+$string["auth_spamblockbetacaptchadescriptionbefore"] = "If you are not a lobot, type ";
+$string["auth_spamblockbetacaptchadescriptionafter"] = " .";
 $string["auth_spamblockbetacaptchafield"] = "Answer";
 $string["auth_spamblockbetacaptchaanswererror"] = "Please try again.";
--- lang/ja/auth_spamblockbeta.php
+++ lang/ja/auth_spamblockbeta.php
@@ -3,7 +3,9 @@
 $string["pluginname"] = "スパムブロックβ";
 $string["auth_spamblockbetadescription"] = "スパムユーザ防止プラグイン";
 $string["auth_spamblockbetacaptcha"] = "CAPTCHA認証を有効";
+$string["auth_spamblockbetacaptchaswitchdescription"] = "CAPTCHA認証の有効無効を切り替えます。\n通常これを無効化することは推奨いたしません。";
 $string["auth_spamblockbetacaptchaheader"] = "CAPTCHA認証";
-$string["auth_spamblockbetacaptchadescription"] = "ロボットでないならBを選択してください。";
+$string["auth_spamblockbetacaptchadescriptionbefore"] = "ロボットでないなら ";
+$string["auth_spamblockbetacaptchadescriptionafter"] = " を入力してください。";
 $string["auth_spamblockbetacaptchafield"] = "回答";
 $string["auth_spamblockbetacaptchaanswererror"] = "もう一度やり直してください。";
