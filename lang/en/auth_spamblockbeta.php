9b9bedf83c6771be6e03b773d21031b2ce8a9851
j19201 <j19201rs@edu.tuis.ac.jp>
2022-09-22 16:10:22 +0900

    メッセージを微変更

--- lang/en/auth_spamblockbeta.php
+++ lang/en/auth_spamblockbeta.php
@@ -5,6 +5,6 @@ $string["auth_spamblockbetadescription"] = "block spam user plugin";
 $string["auth_spamblockbetaanswerlength"] = "Answer length setting for CAPTCHA authentication";
 $string["auth_spamblockbetaanswerlengthdescription"] = "Set the length of the answer for CAPTCHA authentication.\nNormally, 5 characters is recommended.";
 $string["auth_spamblockbetacaptchaheader"] = "CAPTCHA Authentication";
-$string["auth_spamblockbetacaptchadescription"] = "If you are not a lobot, type image words.";
+$string["auth_spamblockbetacaptchadescription"] = "If you are not a lobot, type image words.(You can move the image by dragging.)";
 $string["auth_spamblockbetacaptchafield"] = "Answer";
 $string["auth_spamblockbetacaptchaanswererror"] = "Please try again.";
--- lang/ja/auth_spamblockbeta.php
+++ lang/ja/auth_spamblockbeta.php
@@ -5,6 +5,6 @@ $string["auth_spamblockbetadescription"] = "スパムユーザ防止プラグイ
 $string["auth_spamblockbetaanswerlength"] = "CAPTCHA認証時の答えの長さ設定";
 $string["auth_spamblockbetaanswerlengthdescription"] = "CAPTCHA認証時の答えの長さを設定します。\n通常は5文字がおすすめです。";
 $string["auth_spamblockbetacaptchaheader"] = "CAPTCHA認証";
-$string["auth_spamblockbetacaptchadescription"] = "ロボットでないなら画像の文字を入力してください。";;
+$string["auth_spamblockbetacaptchadescription"] = "ロボットでないなら画像の文字を入力してください。(ドラッグで画像を動かせます。)";;
 $string["auth_spamblockbetacaptchafield"] = "回答";
 $string["auth_spamblockbetacaptchaanswererror"] = "もう一度やり直してください。";
